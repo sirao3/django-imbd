5b3cda5bc655e3f8ce6c5ba31a743f2f4226284b
Sin <sindhoorarao3@gmail.com>
2025-10-20 11:41:24 +0530

    Adds Token auth for registration with automatic token

---
+++ user_app/api/views.py
@@ -0,0 +1,23 @@
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.authtoken.models import Token
+
+from user_app.api.serializers import RegistrationSerializer 
+from user_app import models
+
+@api_view(['POST',])
+def registration_view(request):
+    if request.method == 'POST':
+        serializer = RegistrationSerializer(data=request.data)
+        data = {}
+        if serializer.is_valid():
+            account = serializer.save()
+            data['response'] = "Registration successful"
+            data['username'] = account.username
+            data['email'] = account.email
+
+            token = Token.objects.get(user = account).key
+            data['token'] = token
+            return Response(data)
+        else:
+            return Response(serializer.errors, status=400)
